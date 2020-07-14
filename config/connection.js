6abf9705edfc66dbbec7556dbbc755fc846c0b51
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-07-14 14:56:03 -0400

    added all folders and files and fixed the orm.js

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB);
+} else {
+  connection =  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "6646023Ava!",
+    database: "burgers_db"
+  });
+};
+
+connection.connect();
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Southwest Burger", true), ("Burnin Love Burger", true), ("BBQ Burger", false);
