416995cb67440f68cd558d580a13e1d22ca42ef9
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-07-14 22:30:49 -0400

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWSDB);
+var connection = mysql.createConnection(process.env.JAWSDB);
 } else {
-  connection =  mysql.createConnection({
+  var connection =  mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
